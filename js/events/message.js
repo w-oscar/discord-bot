9971204e2e238c37153ce7c75374e2faae291cf6
Oscar Wos (Laptop) <theoscar@protonmail.com>
2018-07-17 23:55:10 +0100

    Added basic-language support, Improved util.findTarget

--- js/events/message.js
+++ js/events/message.js
@@ -1,12 +1,10 @@
 const util = require('../util.js')
 
 module.exports = async (message) => {
+  if (message.author.bot) return
   util.tryLogToConsole(1, `${message.author.tag} -> ${message.content}`)
 
-  try {
-    let user = await util.checkTarget(message, message.content)
-    console.log(`User Found: ${user.id}`)
-  } catch (e) {
-
-  }
+  await util.checkTarget(message, message.content)
+  .then((guildMember) => message.channel.send(`Target: ${guildMember.user.username}`))
+  .catch((e) => message.channel.send(e))
 }
--- js/util.js
+++ js/util.js
@@ -19,16 +19,50 @@ function tryLogToConsole (type, value) {
 }
 
 function checkTarget (message, phrase) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     if (phrase.startsWith('<@')) {
       phrase = phrase.replace('!', '')
       phrase = phrase.slice(2).slice(0, -1)
     }
 
+    if (message.guild.available) {
+      try {
+        let guildMembers = message.guild.members
+
+        if (message.guild.large) guildMembers = await message.guild.fetchMembers
+        let members = guildMembers.filter((guildMember) => guildMember.displayName.toLowerCase().includes(phrase.toLowerCase())).array()
+
+        if (members.length > 1) {
+          let sendMessage = `${translateMessage('en', 'findTargetMultiple')}\`\`\``
+
+          for (let i = 0; i < members.length || i > 6; i++) {
+            sendMessage += `\n${i} | ${members[i].displayName} (${members[i].user.tag}) [${members[i].id}]`
+          }
+
+          sendMessage += `\`\`\``
+          message.channel.send(sendMessage)
+        } else {
+          if (members.length === 0) reject(translateMessage('en', 'findTargetNoneFound'))
+          if (members.length === 1) resolve(members[0])
+        }
+      } catch (e) {
+        // console.log(e)
+      }
+    } else {
+
+    }
+
+    /*
     index.client.fetchUser(phrase)
     .then((user) => resolve(user))
     .catch((error) => reject(error))
+    */
   })
+}
+
+function translateMessage (language, phrase) {
+  let languageFile = require(`../language/${language}`)
+  return languageFile[`${phrase}`]
 }
 
 module.exports = { start, tryLogToConsole, checkTarget }
